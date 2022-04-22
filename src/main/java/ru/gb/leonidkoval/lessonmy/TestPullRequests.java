f8ed33456d5109fd13b036634a1331ac92f940a5
HP <orange59rus@mail.ru>
2022-04-22 16:09:39 +0300

    проверяю наконец-то пулреквест

--- src/main/java/ru/gb/leonidkoval/lessonmy/TestPullRequests.java
+++ src/main/java/ru/gb/leonidkoval/lessonmy/TestPullRequests.java
@@ -3,5 +3,6 @@ package ru.gb.leonidkoval.lessonmy;
 public class TestPullRequests {
     public static void main(String[] args) {
         System.out.println("руссий язык виден отчетливо");
+        //уцауцпупкуп
     }
 }
