d8dc1ce9c876258ed6480e81fd9da8a4f62f3ebf
HP <orange59rus@mail.ru>
2022-04-24 20:34:48 +0300

    продолжаем игру

--- src/main/java/ru/gb/leonidkoval/homework4/MineSweepper.java
+++ src/main/java/ru/gb/leonidkoval/homework4/MineSweepper.java
@@ -1,15 +1,30 @@
 package ru.gb.leonidkoval.homework4;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class MineSweepper {
     //неизменяемые значения (константы - final, имя ЗАГЛАВНЫМИ БУКВАМИ):
     //тк объявили их в классе то их будет видно вовсех методах
-    public static final int WIDTH = 10;            //ширина игр поля
+    public static final int WIDTH = 15;            //ширина игр поля
     public static final int HEIGTH = 10;           //высота игр поля
-    public static final int MINES_COUNT = 99;            // колво мин
+    public static final int MINES_COUNT = 20;            // колво мин
+
     public static final int MINE = 1000;
     public static final int EMPTY = 0;
+    public static final int CELL_OPEN = 1;
+    public static final int CELL_CLOSE = 0;
+    public static final int CELL_FLAG = -1;
+
+
+    public static final String ANSI_RESET = "\u001b[0m";
+    public static final String ANSI_RED = "\u001b[31m";
+    public static final String ANSI_GREEN = "\u001b[32m";
+    public static final String ANSI_YELOW = "\u001b[33m";
+    public static final String ANSI_BLUE = "\u001b[34m";
+    public static final String ANSI_PURPLE = "\u001b[35m";
+    public static final String ANSI_CYAN = "\u001b[36m";
+    public static final String ANSI_WHITE = "\u001b[37m";
 
 
 
@@ -24,7 +39,22 @@ public class MineSweepper {
     public static void play() {
         boolean win = true;
         int[][] board = generateBoard();            //создаёт игровое поле
-        printBoard(board);                          //печать игрового поля
+        int[][] moves = new int [HEIGTH][WIDTH];
+        boolean isPassMove = move(board, moves);    // если  не взорвался true
+        //printBoard(board, moves);                          //печать игрового поля
+    }
+
+    private static boolean move(int[][] board, int[][] moves) {
+        Scanner scanner = new Scanner(System.in);
+        printBoard(board, moves);
+        int row, line;  //столбец и строка
+        while(true) {
+            System.out.print("ВАШ ХОД (СТРОКА, СТОЛБЕЦ, например 1А): ");
+            String move = scanner.nextLine();
+            char firstSym = move.charAt(0);
+            char secondSym = move.charAt(1);
+        }
+        return false;
     }
 
     public static int[][] generateBoard() {
@@ -65,10 +95,10 @@ public class MineSweepper {
         return board;
         }
 
-    public static void printBoard(int[][] board) { // печать игровой доски в методе
+    public static void printBoard(int[][] board, int[][] moves) { // печать игровой доски в методе
         System.out.println();
         System.out.println("  SUPER MEGA DANGER GAME ");
-        System.out.println("      *S*A*P*P*E*R*");
+        System.out.println("      \u001b[31m*S*A*P*P*E*R*\u001b[0m"); //сделал красным и отменил красный цвет текста после его вывода
         System.out.println("    !!DON'T EXPLODE!!");
         System.out.println();
         System.out.print("  /");
@@ -79,6 +109,16 @@ public class MineSweepper {
         for (int i = 0; i < HEIGTH; i++) {
             System.out.printf("%3d", i);
             for (int j = 0; j < WIDTH; j++) {
+                if (moves[i][j] == CELL_CLOSE) {          //проверяем пустая ли ячейка
+                    System.out.print("[]");
+                    continue;
+                }
+                if (moves[i][j] == CELL_FLAG){
+                    System.out.print(" P");  //флажок
+                    continue;
+                }
+                final String colorCode = getColorCode(board[i][j]);
+                System.out.print(colorCode);
                 if (board[i][j] == EMPTY) {
                     System.out.print(" .");   // если ноль то печатаем тчк
                 } else if (board[i][j] == MINE) {
@@ -86,10 +126,32 @@ public class MineSweepper {
                 } else {
                     System.out.printf("%2d", board[i][j]); // если там цифра то напечатаем её используя 2места в строке тк %2d
                 }
+                System.out.print(ANSI_RESET); //сброс цвета
             }
             System.out.println();
 
         }
 
     }
+
+    public static String getColorCode(int i) {
+    switch (i) {
+        case EMPTY:
+            return ANSI_WHITE;
+        case MINE:
+            return ANSI_PURPLE;
+        case 1:
+            return ANSI_BLUE;
+        case 2:
+            return ANSI_GREEN;
+        case 3:
+            return ANSI_RED;
+        case 4:
+            return ANSI_CYAN;
+        default:               //если 5,6,7,8
+            return ANSI_YELOW;
+    }
+
+    }
+
 }
