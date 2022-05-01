9b8b40370d79b4dc2880dda610425db66929a8e3
HP <orange59rus@mail.ru>
2022-05-01 19:09:10 +0300

    youtub4ik

--- src/main/java/alishev_you_tube/QConstructor.java
+++ src/main/java/alishev_you_tube/QConstructor.java
@@ -1,4 +1,38 @@
 package alishev_you_tube;
 
 public class QConstructor {
+    public static void main(String[] args) {
+        Human1 human1 = new Human1();
+
+
+    }
+}
+class Human1 {
+    private String name;
+    private int age;
+
+    public Human1(){
+        this.name = "имя по умолчанию";
+        this.age = 0;
+        System.out.println(name + " " + age);
+    }
+    public Human1(String name) {
+        System.out.println("привет из 2го конструктора");
+        this.name = name;
+    }
+    public Human1(String name, int age){
+        System.out.println("привет из 3го конструктора");
+        this.age = age;
+        this.name = name;
+
+
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
