1b10627434df6e76230f12fbe7c700a7668d2433
HP <orange59rus@mail.ru>
2022-05-16 16:55:41 +0300

    hw6

--- src/main/java/ru/gb/leonidkoval/hw5/Employee.java
+++ src/main/java/ru/gb/leonidkoval/hw5/Employee.java
@@ -1,5 +1,6 @@
 package ru.gb.leonidkoval.hw5;
 public class Employee {
+
   private String name;
   private String position;
   private String email;
@@ -7,7 +8,7 @@ public class Employee {
   private int salary;
   private int age;
 
-public Employee(String name, String position, String email, int numPhoneWork, int salary, int age) {
+  public Employee(String name, String position, String email, int numPhoneWork, int salary, int age) {
     this.name = name;
     this.position = position;
     this.email = email;
@@ -15,20 +16,31 @@ public Employee(String name, String position, String email, int numPhoneWork, in
     this.salary = salary;
     this.age = age;
 }
-public int getAge()  {
-    return age;
-}
 
     @Override
-   public String toString() {
-       return "Employ{" +
-               "name='" + name + '\'' +
-               ", position='" + position + '\'' +
-               ", email='" + email + '\'' +
-               ", numPhoneWork=" + numPhoneWork +
-               ", salary=" + salary +
-               ", age=" + age +
-               '}';
-   }
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", numPhoneWork=" + numPhoneWork +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getAge()  {
+        return age;
+    }
+
+// toString лучше и быстрее этого
+    //public void print() {
+    //System.out.println("name= " + name +
+    //        ", position= " + position +
+    //        ", email= " + email +
+    //        ", numPhoneWork= " + numPhoneWork +
+    //        ", salary= " + salary +
+    //        ", age=" + age);
+    //  }
 
   }
