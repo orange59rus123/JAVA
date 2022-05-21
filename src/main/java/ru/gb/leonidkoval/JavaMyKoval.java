252b650d24bfbb557a27003cc54f54f0f9b9a6ad
HP <orange59rus@mail.ru>
2022-05-21 12:23:18 +0300

    проверка

--- src/main/java/ru/gb/leonidkoval/JavaMyKoval.java
+++ src/main/java/ru/gb/leonidkoval/JavaMyKoval.java
@@ -3,5 +3,6 @@ package ru.gb.leonidkoval;
 public class JavaMyKoval {
     public static void main(String[] args){
         System.out.println("видишь русский язык yes no ");
+        System.out.println("видишь русский язык yes no ");
     }
 }
