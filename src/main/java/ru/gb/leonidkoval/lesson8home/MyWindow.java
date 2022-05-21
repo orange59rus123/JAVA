a30f591dc163cd7ab0d179ab899e82dd01ff02ac
HP <orange59rus@mail.ru>
2022-05-21 13:28:37 +0300

    название кнопок меняется при нажатии циклом

--- src/main/java/ru/gb/leonidkoval/lesson8home/MyWindow.java
+++ src/main/java/ru/gb/leonidkoval/lesson8home/MyWindow.java
@@ -10,17 +10,21 @@ public class MyWindow extends JFrame {  // с помощью extends JFrame им
         setTitle("test Window");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);// при закрытии окошка программа завершается
         setBounds(450, 200, 400, 400);//гранцы окна лев верх угол, ширина, высота
-        final JButton button1 = new JButton("Button #1");  //кнопки с названием Button #,,
-        final JButton button2 = new JButton("Button #2");  //кнопки
-        final JButton button3 = new JButton("Button #3");  //кнопки
-        final JButton button4 = new JButton("Button #4");  //кнопки
-        final JButton button5 = new JButton("Button #5");  //кнопки
 
-        add(button1, BorderLayout.NORTH); //вызов кнопки с расположением BorderLayout
-        add(button2, BorderLayout.SOUTH);
-        add(button3, BorderLayout.WEST);
-        add(button4, BorderLayout.EAST);
-        add(button5, BorderLayout.CENTER);
+        final JButton[] jButtons = new JButton[5];
+
+        jButtons[0] = new JButton("Button #1");  //кнопки с названием Button #,,
+        jButtons[1] = new JButton("Button #1");  //кнопки с названием Button #,,
+        jButtons[2] = new JButton("Button #2");  //кнопки
+        jButtons[3] = new JButton("Button #3");  //кнопки
+        jButtons[4] = new JButton("Button #4");  //кнопки
+
+
+        add(jButtons[0], BorderLayout.NORTH); //вызов кнопки с расположением BorderLayout
+        add(jButtons[1], BorderLayout.SOUTH);
+        add(jButtons[2], BorderLayout.WEST);
+        add(jButtons[3], BorderLayout.EAST);
+        add(jButtons[4], BorderLayout.CENTER);
 
         final ActionListener btnListener = new ActionListener() { //для работы кнопки
             //при нажатии 5 нопки на остальных кнопках подпись "Нажал на кнопку 5"
@@ -29,18 +33,13 @@ public class MyWindow extends JFrame {  // с помощью extends JFrame им
                 final JButton button = (JButton) e.getSource();
                 final String text = button.getText();
                 final String btnNumber = text.substring(text.length() - 1);
-                button1.setText("Нажал на кнопку " + btnNumber);
-                button2.setText("Нажал на кнопку " + btnNumber);
-                button3.setText("Нажал на кнопку " + btnNumber);
-                button4.setText("Нажал на кнопку " + btnNumber);
-                button5.setText("Нажал на кнопку " + btnNumber);
+
             }
         };
-        button1.addActionListener(btnListener);//при нажатии на кнопку название кнопок меняется "Нажал на кнопку " + button
-        button2.addActionListener(btnListener);
-        button3.addActionListener(btnListener);
-        button4.addActionListener(btnListener);
-        button5.addActionListener(btnListener);
+        for (JButton jButton : jButtons) { //циклом определяем нажатие кнопок
+            jButton.addActionListener(btnListener);
+        }
+
 
         setVisible(true); //чтобы окно появилось
     }
