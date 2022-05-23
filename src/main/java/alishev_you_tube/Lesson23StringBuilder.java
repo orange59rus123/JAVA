f1a4169f18ba96c1a0169d8dcec0ddb40c4e9d54
HP <orange59rus@mail.ru>
2022-05-23 20:03:29 +0300

    youtub4ik

--- src/main/java/alishev_you_tube/Lesson23StringBuilder.java
+++ src/main/java/alishev_you_tube/Lesson23StringBuilder.java
@@ -1,7 +1,33 @@
 package alishev_you_tube;
 
+import org.w3c.dom.ls.LSOutput;
+
+import javax.print.DocFlavor;
+import java.util.Locale;
+
 public class Lesson23StringBuilder {
     public static void main(String[] args) {
+        String x = "hello";
+
+        System.out.println(x);
+        x = x.toUpperCase(); // toUpperCase() из маленьких букв делает загавные но нужно присвоить переменной
+        System.out.println(x);
+
+
+        String s1 = "Hello";
+        String s2 = " my";
+        String s3 = " friend";
+        String sAll = s1 + s2 + s3;
+        System.out.println(sAll);
+
+               //StringBuilder для быстроты при прибавлении строк
+        StringBuilder sb = new StringBuilder("Hello StringBuilder");
+        System.out.println(sb.toString());
+        sb.append(" plus String1");      //прибавление к строке StringBuilder
+        sb.append(" and plus String2");  //прибавление к строке StringBuilder
+        System.out.println(sb.toString());
+        sb.append(" plus String3").append(" and plus String4");  //  укоротили
+        System.out.println(sb.toString());
 
     }
 }
